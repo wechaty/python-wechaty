514c83529b028a3262595b52125406cdce83c80a
Huan LI <zixia@zixia.net>
2020-02-29 23:35:11 +0800

    add VERSION

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
-""" setup """
+''' setup '''
 
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+version = '0.0.0'
+with open('VERSION', 'r') as fh:
+    version = fh.readline()
+
 setuptools.setup(
-    name="wechaty",
-    version="0.0.2",
-    author="Huan LI",
-    author_email="zixia@zixia.net",
-    description="Wechaty is a Bot SDK for Wechat Personal Account",
+    name='wechaty',
+    version=version,
+    author='Huan LI (李卓桓)',
+    author_email='zixia@zixia.net',
+    description='Wechaty is a Bot SDK for Wechat Personal Account',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/Chatie/python-wechaty",
+    long_description_content_type='text/markdown',
+    url='https://github.com/Chatie/python-wechaty',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
     ],
 )
