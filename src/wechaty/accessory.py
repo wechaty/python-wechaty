98eedc7d39607bac67ff4b43b209faf0704d6620
Huan LI <zixia@zixia.net>
2020-03-07 01:50:32 +0800

    rewrite class Accessory, add unit test

--- src/wechaty/accessory.py
+++ src/wechaty/accessory.py
@@ -1,83 +1,89 @@
 """
 docstring
 """
-from abc import ABCMeta
-from typing import Optional
-from wechaty_puppet.puppet import Puppet
-from .config import LOG
+# from abc import ABC
+# from typing import (
+#     overload,
+#     Optional,
+# )
+from wechaty_puppet import Puppet
+from .config import (
+    logging
+)
 from .wechaty import Wechaty
 
+log = logging.getLogger('Accessory')
 
-class Accessory:
+
+class AccessoryMeta(type):
+    """docs"""
+
+    _static_puppet : Puppet  = None
+    _static_wechaty: Wechaty = None
+
+    @property
+    def puppet(cls) -> Puppet:
+        """doc"""
+        if cls._static_puppet is None:
+            raise AttributeError('static puppet not found ...')
+
+        return cls._static_puppet
+
+    @puppet.setter
+    def puppet(cls, new_puppet: Puppet) -> None:
+        if cls._static_puppet is not None:
+            raise AttributeError('static puppet can not be set twice')
+
+        cls._static_puppet = new_puppet
+
+    @property
+    def wechaty(cls) -> Wechaty:
+        """doc"""
+        if cls._static_wechaty is None:
+            raise AttributeError('static wechaty not found ...')
+        return cls._static_wechaty
+
+    @wechaty.setter
+    def wechaty(cls, new_wechaty) -> None:
+        if cls._static_wechaty is not None:
+            raise AttributeError(
+                'static wechaty can not be set twice'
+            )
+        cls._static_wechaty = new_wechaty
+
+
+class Accessory(metaclass=AccessoryMeta):
     """
     docstring
     """
-    __metaclass__ = ABCMeta
-    _puppet: Optional[Puppet] = None
-    # static _wechaty property to doing ...
-    _wechaty: Optional[Wechaty] = None
-    _counter: int = 0
 
-    def __init__(self, name: str = "accessory"):
-        """
-        initialize the accessory instance
-        """
-        self.name: str = name
-        # increase when Accessory is initialized
-        self._counter += 1
+    _puppet : Puppet  = None
+    _wechaty: Wechaty = None
 
-    def __str__(self) -> str:
-        """
-        docstring
-        :return: the base accessory class name
-        """
-        return "Accessory instance : %s" % self.name
+    @property
+    def puppet(self) -> Puppet:
+        """doc"""
+        if self._puppet is not None:
+            return self._puppet
 
-    @classmethod
-    def puppet(cls, value: Optional[Puppet] = None) -> Optional[Puppet]:
-        """
-        get/set global single instance of the puppet
-        :return:
-        """
-        if value is None:
-            if cls._puppet is None:
-                raise AttributeError("static puppet instance not found ...")
-            LOG.info("get puppet instance %s ...",
-                     cls._puppet.name)
-            return cls._puppet
+        return self.__class__.puppet
 
-        if cls._puppet is not None:
-            raise AttributeError("can't set puppet instance %s twice" %
-                                 cls._puppet.name)
-        LOG.info("set puppet instance %s ...",
-                 value.name)
-        cls._puppet = value
-        return None
+    @puppet.setter
+    def puppet(self, new_puppet) -> None:
+        if self._puppet is not None:
+            raise AttributeError('puppet can not be set twice')
 
-    @classmethod
-    def wechaty(cls, value: Optional[Wechaty] = None) -> Optional[Wechaty]:
-        """
-        get/set wechaty instance
+        self._puppet = new_puppet
 
-        If the param of value is None, then the function will return the
-        instance of wechaty.Otherwise, the function will check the type
-        of the value, and set as wechaty instance
-        :param value:
-        :return:
-        """
-        if value is None:
-            if cls._wechaty is None:
-                raise AttributeError("wechaty instance not found")
-            LOG.info("get wechaty instance %s",
-                     cls._wechaty.name)
-            return cls._wechaty
-        if not isinstance(value, Wechaty):
-            raise NameError(
-                "expected wechaty instance type is Wechaty, "
-                "but got %s" % value.__class__
-            )
-        if cls._wechaty is not None:
-            raise AttributeError("can't set wechaty instance %s twice" %
-                                 cls._wechaty.name)
-        cls._wechaty = value
-        return None
+    @property
+    def wechaty(self) -> Wechaty:
+        """doc"""
+        if self._wechaty is not None:
+            return self._wechaty
+        return self.__class__.wechaty
+
+    @wechaty.setter
+    def wechaty(self, new_wechaty) -> None:
+        if self._wechaty is not None:
+            raise AttributeError('wechaty can not be set twice')
+        self._wechaty = new_wechaty
---
+++ src/wechaty/accessory_test.py
@@ -0,0 +1,100 @@
+"""
+accessory unit test
+"""
+from typing import (
+    cast,
+)
+import pytest
+
+from wechaty_puppet import (
+    Puppet,
+)
+from .accessory import (
+    Accessory,
+)
+
+EXPECTED_PUPPET1 = cast(Puppet, {'p': 1})
+EXPECTED_PUPPET2 = cast(Puppet, {'p': 2})
+
+EXPECTED_WECHATY1 = {'w': 1}
+EXPECTED_WECHATY2 = {'w': 1}
+
+
+def test_accessory_read_uninitialized_static_wechaty_puppet(
+        accessory_class: Accessory,
+):
+    """should throw if read static wechaty & puppet before initialization"""
+
+    with pytest.raises(Exception) as e:
+        assert accessory_class.puppet
+    assert str(e.value) == 'static puppet not found ...'
+
+    with pytest.raises(Exception) as e:
+        assert accessory_class.wechaty
+    assert str(e.value) == 'static wechaty not found ...'
+
+
+def test_accessory_read_initialized_class_wechaty_puppet(
+        accessory_class: Accessory,
+):
+    """
+    should read excepted value by reading static wechaty & puppet after init
+    """
+
+    accessory_class.puppet  = EXPECTED_PUPPET1
+    accessory_class.wechaty = EXPECTED_WECHATY1
+
+    assert \
+        accessory_class.puppet == EXPECTED_PUPPET1, \
+        'should get puppet back'
+    assert \
+        accessory_class.wechaty == EXPECTED_WECHATY1, \
+        'should get wechaty back'
+
+
+def test_accessory_read_uninitialized_instance_wechaty_puppet(
+        accessory_class,
+):
+    """should throw if read instance wechaty & puppet before initialization"""
+    # pytest.skip('tbd')
+
+    instance = accessory_class()
+
+    with pytest.raises(Exception) as e:
+        print(instance.puppet)
+        assert instance.puppet
+    assert str(e.value) == 'static puppet not found ...'
+
+    with pytest.raises(Exception) as e:
+        assert instance.wechaty
+    assert str(e.value) == 'static wechaty not found ...'
+
+
+def test_accessory_read_initialized_instance_wechaty_puppet(
+        accessory_class: Accessory,
+):
+    """
+    should get expected value by reading instance wechaty & puppet after init
+    """
+
+    accessory_instance = accessory_class()
+
+    accessory_instance.puppet  = EXPECTED_PUPPET1
+    accessory_instance.wechaty = EXPECTED_WECHATY1
+
+    assert \
+        accessory_instance.puppet == EXPECTED_PUPPET1, \
+        'should get puppet back'
+    assert \
+        accessory_instance.wechaty == EXPECTED_WECHATY1, \
+        'should get wechaty back'
+
+
+@pytest.fixture(name='accessory_class')
+def fixture_accessory_class():
+    """create a fixture"""
+
+    class FixtureClass(Accessory):
+        """fixture"""
+
+    yield FixtureClass
