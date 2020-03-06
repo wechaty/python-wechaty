2aeb9363791825a3f5a288a7202acedb8ddf0a8d
Huan LI <zixia@zixia.net>
2020-03-07 01:58:36 +0800

    add demo example

---
+++ examples/demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3.8
+"""doc"""
+import asyncio
+
+from wechaty_puppet import FileBox
+
+
+async def main():
+    '''doc'''
+    print('hello')
+    await asyncio.sleep(1)
+    print('world')
+    f = FileBox()
+    print(await f.to_file('t.dat'))
+
+
+asyncio.run(main())
