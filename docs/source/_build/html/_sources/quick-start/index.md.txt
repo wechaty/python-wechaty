23ed8ab8c3fe0327470e81f6b4b31ab3a6d9f065
wjmcat <1435130236@qq.com>
2020-07-12 13:46:08 +0800

    init config for sphinx

---
+++ docs/source/_build/html/_sources/quick-start/index.md.txt
@@ -0,0 +1,5 @@
+# quick start for python-wechaty
+
+## Install packages
+
+> we should install all of the package first
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,19 +10,22 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'python-wechaty-docs'
+project = 'python-wechaty'
 copyright = '2020, wj-Mcat'
 author = 'wj-Mcat'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.6'
+version_file = '../../VERSION'
+release = '0.0.1'
+with open(version_file, 'r', encoding='utf-8') as f:
+    release = f.readline()
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,7 +34,58 @@ release = '0.6.6'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.katex',
+    'sphinx.ext.autosectionlabel',
+    'sphinx_rtd_theme',
+    'recommonmark'
 ]
+
+# build the templated autosummary files
+autosummary_generate = True
+numpydoc_show_class_members = False
+
+# autosectionlabel throws warnings if section names are duplicated.
+# The following tells autosectionlabel to not throw a warning for
+# duplicated section names that are in different documents.
+autosectionlabel_prefix_document = True
+
+# katex options
+#
+#
+
+katex_prerender = True
+
+napoleon_use_ivar = True
+
+# TODO: document these and remove them from here.
+
+coverage_ignore_modules = [
+]
+
+coverage_ignore_functions = [
+]
+
+coverage_ignore_classes = [
+]
+
+master_doc = 'index'
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -41,15 +95,20 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
