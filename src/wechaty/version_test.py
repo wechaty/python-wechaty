1621214be6fe53ef90f13d7916a87f11ed50bee2
wjmcat <1435130236@qq.com>
2021-03-04 21:59:31 +0800

    fix version test

--- src/wechaty/version_test.py
+++ src/wechaty/version_test.py
@@ -4,11 +4,17 @@ version unit test
 # import pytest   # type: ignore
 
 from .version import VERSION
+import os
 
 
 def test_version() -> None:
     """
     Unit Test for version file
     """
+    version_file = os.path.join(os.getcwd(), 'VERSION')
+    if not os.path.exists(version_file):
+        raise FileNotFoundError('VERSION file not found')
+    with open(version_file, 'r') as f:
+        version = f.read().strip()
 
-    assert VERSION == '0.0.0', 'version should be 0.0.0'
+    assert VERSION == version, 'version should be consist with VERSION file'
