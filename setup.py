3af625a0574c4fde24c35731af8827147c08ccb3
wj-Mcat <1435130236@qq.com>
2022-07-18 23:08:14 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -78,7 +78,6 @@ setuptools.setup(
     url='https://github.com/wechaty/python-wechaty',
     packages=setuptools.find_packages('src'),
     package_dir={'wechaty': 'src/wechaty'},
-    include_package_data=True,
     install_requires=get_install_requires(),
     classifiers=[
         'Programming Language :: Python :: 3.7',
