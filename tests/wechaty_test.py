ad7dd5b0c647e9a9102ee1599a96f64d42cc8af4
wj-Mcat <1435130236@qq.com>
2022-11-20 15:22:12 +0800

    add unittest for wechaty

--- tests/wechaty_test.py
+++ tests/wechaty_test.py
@@ -3,6 +3,7 @@ Unit test
 """
 import pytest
 import os
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from wechaty_puppet import WechatyPuppetConfigurationError
 from wechaty import Wechaty, WechatyOptions 
 
@@ -19,3 +20,13 @@ def test_constructor():
     
     assert bot.puppet.options.token == 'fake-token'
     assert bot.puppet.options.end_point == '127.0.0.1:8080'
+
+
+def test_scheduler():
+    """test add scheduler in the options"""
+    scheduler = AsyncIOScheduler()
+    options = WechatyOptions(
+        scheduler=scheduler
+    )
+    bot = Wechaty(options=options)
+    assert id(scheduler) == id(bot._plugin_manager.scheduler)
